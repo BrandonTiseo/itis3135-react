f66a3f47f354646afaea6519664e4c7f8a70acc0
BrandonTiseo <Brandontiseo@yahoo.com>
2025-11-05 20:51:11 -0500

    fixed header links

--- src/header.jsx
+++ src/header.jsx
@@ -3,11 +3,11 @@ export default function Header(){
         <header>
             <h1>Brandon Tiseo Bold Turtle || ITIS3135</h1>
             <nav>
-                <a href="./">Home</a>
+                <a href="/">Home</a>
                 |
-                <a href="./introduction">Introduction</a>
+                <a href="/introduction">Introduction</a>
                 |
-                <a href="./contract">Contract</a>
+                <a href="/contract">Contract</a>
             </nav>
         </header>
     );
